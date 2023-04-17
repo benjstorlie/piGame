a41f3e86d37b582e7d961e5ac90293b49b30083c
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2023-04-16 19:27:11 -0700

    re-did the keys so they are all jQuery buttons

--- script.js
+++ script.js
@@ -6,68 +6,39 @@ let index = 0;
 // digit refers to which digit to compare to pi.
 let digit;
 
-// The following variables are to hold the state of which key is moused over
-// and which key is currently pressed down.
-// These will be changed by the events mouseover, mouseleave, keydown, and keyup.
-// The number 12 is to indicate that none of the keys are selected,
-// and the numbers 0-11 will indicate that that key is selected.
-// keydown is an array of booleans since more than one key can be pressed at a time.
-let mouse=12;
-let keydown=[false,false,false,false,false,false,false,false,false,false];
-
 // This is the a string representing the keys on the keyboard in order
-const keyboardString = "0123456789. ";
-const keyboard = document.getElementById("keyboard");
-
-
-displayKeyboard();
-
-function displayKeyboard() {
-  // Create the number keyboard grid
-
+const keyboardString = "0123456789";
   // orderArray shows the order that the keys will be displayed, to match a typical numpad.
-  const orderArray = [10,6,7,8,3,4,5,0,1,2,11,9]
-  for (i=0; i<12; i++) {
-    let key = document.createElement("div");
-    key.className = "key";
-    key.style.order=orderArray[i];
-    key.style.borderWidth="2px 4px 4px 2px";
-    key.textContent = keyboardString[i];
-    keyboard.appendChild(key);
+const orderArray = [10,6,7,8,3,4,5,0,1,2,11,9];
+
+$(function() {
+  const keyboard = $("#keyboard");
+
+  for (i=0; i<10; i++) {
+    keyboard.append(addKey(i));
   }
-}
+  const backButton = $("<button>").attr("id","back-button").addClass("key").css("order","9");
+  backButton.click(function() {console.log("back")});
+  keyboard.append(backButton);
 
-//for (i=0; i<12; i++) {
-//  keyboard.children[i].addEventListener("mouseover", function(){
-//    mouse = i;
-//    keydownDisplay(i);
-//  });
-//  keyboard.children[i].addEventListener("mouseleave", function(){
-//    mouse = 12;
-//    keyupDisplay(i);
-//  });
-//}
-
-document.addEventListener("keydown", function(event) {
-  keydown[event.key]=true;
-  keydownDisplay(event.key);
+  $(document).keydown( function(event) {
+    if (!isNaN(event.key)) {$("#key"+event.key).click()}; 
+    console.log(event.key);
+  })
 });
 
-document.addEventListener("keyup", function(event) {
-  digit = event.key; // The key entered to be compared to pi
-  keydown[event.key]=false; // Indicating the key is no longer pressed.
-  keyupDisplay(event.key);
-});
+function addKey(i) {
+  const key=$("<button>");
+  key.addClass("key");
+  key.css("order",orderArray[i]);
+  key.attr("id","key"+i)
+  key.text(keyboardString[i]);
 
+  key.click(function() {
+    digit = i;
+    console.log("digit="+digit);
+  })
 
-function keydownDisplay(keyString) {
-  if (!isNaN(Number(keyString))) {
-    keyboard.children[keyString].style.borderWidth="4px 2px 2px 4px";
-  }
+  return key;
 }
 
-function keyupDisplay(keyString) {
-  if (!isNaN(Number(keyString))) {
-    keyboard.children[keyString].style.borderWidth="2px 4px 4px 2px";
-  } 
-}
